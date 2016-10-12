397e5e884ebae3e9c37a65a4d80b6b4da4ac39a1
Paul Stead <paul.stead@zeninternet.co.uk>
2016-10-12 22:52:53 +0100

    0.4 release
     - Added **check_olemacro_renamed**, **check_olemacro_zip_password** and **check_olemacro_encrypted**
     - Added configuration options
     - Adjusted zip support
     - More dbg'ing

--- OLEMacro.pm
+++ OLEMacro.pm
@@ -12,22 +12,12 @@ use re 'taint';
 use vars qw(@ISA);
 @ISA = qw(Mail::SpamAssassin::Plugin);
 
-our $VERSION = '0.321';
+our $VERSION = '0.4';
 
 # https://www.openoffice.org/sc/compdocfileformat.pdf
 # http://blog.rootshell.be/2015/01/08/searching-for-microsoft-office-files-containing-macro/
 my $marker1 = "\xd0\xcf\x11\xe0";
 my $marker2 = "\x00\x41\x74\x74\x72\x69\x62\x75\x74\x00";
-
-# https://blogs.msdn.microsoft.com/vsofficedeveloper/2008/05/08/office-2007-file-format-mime-types-for-http-content-streaming-2/
-# https://technet.microsoft.com/en-us/library/ee309278(office.12).aspx
-my $macrotypes = qr/(?:docm|dotm|ppam|potm|ppst|ppsm|pptm|sldm|xlam|xlsb|xlsm|xltm)$/;
-my $exts = qr/(?:doc|dot|pot|ppa|pps|ppt|xla|xls|xlt|zip)$/;
-my $skip_exts = qr/\.(?:docx|dotx|potx|pptx|xlsx)$/;
-
-my $max_mime = 5;
-my $max_zip = 5;
-my $zip_num_of_bytes = 512000;
 
 # constructor: register the eval rule
 sub new {
@@ -39,14 +29,114 @@ sub new {
   my $self = $class->SUPER::new($mailsaobject);
   bless ($self, $class);
 
+  $self->set_config($mailsaobject->{conf});
+
   $self->register_eval_rule("check_olemacro");
   $self->register_eval_rule("check_olemacro_malice");
+  $self->register_eval_rule("check_olemacro_renamed");
+  $self->register_eval_rule("check_olemacro_encrypted");
+  $self->register_eval_rule("check_olemacro_zip_password");
 
   return $self;
 }
 
 sub dbg {
   Mail::SpamAssassin::Plugin::dbg ("OLEMacro: @_");
+}
+
+sub set_config {
+  my ($self, $conf) = @_;
+  my @cmds = ();
+
+  push(@cmds, {
+    setting => 'olemacro_num_mime',
+    default => 5,
+    type => $Mail::SpamAssassin::Conf::CONF_TYPE_NUMERIC,
+  });
+
+  push(@cmds, {
+    setting => 'olemacro_num_zip',
+    default => 5,
+    type => $Mail::SpamAssassin::Conf::CONF_TYPE_NUMERIC,
+  });
+
+  push(@cmds, {
+    setting => 'olemacro_extended_scan',
+    default => 0,
+    type => $Mail::SpamAssassin::Conf::CONF_TYPE_BOOL,
+  });
+
+  push(@cmds, {
+    setting => 'olemacro_max_file',
+    default => 512000,
+    type => $Mail::SpamAssassin::Conf::CONF_TYPE_NUMERIC,
+  });
+
+  # https://blogs.msdn.microsoft.com/vsofficedeveloper/2008/05/08/office-2007-file-format-mime-types-for-http-content-streaming-2/
+  # https://technet.microsoft.com/en-us/library/ee309278(office.12).aspx
+
+  push(@cmds, {
+    setting => 'olemacro_exts',
+    default => '(?:doc|dot|pot|ppa|pps|ppt|xla|xls|xlt)$',
+    type => $Mail::SpamAssassin::Conf::CONF_TYPE_STRING,
+    code => sub {
+      my ($self, $key, $value, $line) = @_;
+      unless (defined $value && $value !~ /^$/) {
+        return $Mail::SpamAssassin::Conf::MISSING_REQUIRED_VALUE;
+      }
+      return $Mail::SpamAssassin::Conf::INVALID_VALUE unless $self->{parser}->is_delimited_regexp_valid('TESTING', $value);
+
+      $self->{olemacro_exts} = $value;
+      },
+    }
+  );
+
+  push(@cmds, {
+    setting => 'olemacro_macro_exts',
+    default => '(?:docm|dotm|ppam|potm|ppst|ppsm|pptm|sldm|xlam|xlsb|xlsm|xltm)$',
+    type => $Mail::SpamAssassin::Conf::CONF_TYPE_STRING,
+    code => sub {
+      my ($self, $key, $value, $line) = @_;
+      unless (defined $value && $value !~ /^$/) {
+        return $Mail::SpamAssassin::Conf::MISSING_REQUIRED_VALUE;
+      }
+      return $Mail::SpamAssassin::Conf::INVALID_VALUE unless $self->{parser}->is_delimited_regexp_valid('TESTING', $value);
+
+      $self->{olemacro_macro_exts} = $value;
+    },
+  });
+
+  push(@cmds, {
+    setting => 'olemacro_skip_exts',
+    default => '(?:docx|dotx|potx|pptx|xlsx)$',
+    type => $Mail::SpamAssassin::Conf::CONF_TYPE_STRING,
+    code => sub {
+      my ($self, $key, $value, $line) = @_;
+      unless (defined $value && $value !~ /^$/) {
+        return $Mail::SpamAssassin::Conf::MISSING_REQUIRED_VALUE;
+      }
+      return $Mail::SpamAssassin::Conf::INVALID_VALUE unless $self->{parser}->is_delimited_regexp_valid('TESTING', $value);
+
+      $self->{olemacro_skip_exts} = $value;
+    },
+  });
+
+  push(@cmds, {
+    setting => 'olemacro_zips',
+    default => '(?:zip)$',
+    type => $Mail::SpamAssassin::Conf::CONF_TYPE_STRING,
+    code => sub {
+      my ($self, $key, $value, $line) = @_;
+      unless (defined $value && $value !~ /^$/) {
+        return $Mail::SpamAssassin::Conf::MISSING_REQUIRED_VALUE;
+      }
+      return $Mail::SpamAssassin::Conf::INVALID_VALUE unless $self->{parser}->is_delimited_regexp_valid('TESTING', $value);
+
+      $self->{olemacro_zips} = $value;
+    },
+  });
+
+  $conf->{parser}->register_commands(\@cmds);
 }
 
 sub check_olemacro {
@@ -65,23 +155,51 @@ sub check_olemacro_malice {
   return $pms->{olemacro_malice};
 }
 
+sub check_olemacro_renamed {
+  my ($self,$pms,$body,$name) = @_;
+
+  _check_attachments(@_) unless exists $pms->{olemacro_renamed};
+
+  return $pms->{olemacro_renamed};
+}
+
+sub check_olemacro_encrypted {
+  my ($self,$pms,$body,$name) = @_;
+
+  _check_attachments(@_) unless exists $pms->{olemacro_encrypted};
+
+  return $pms->{olemacro_encrypted};
+}
+
+sub check_olemacro_zip_password {
+  my ($self,$pms,$body,$name) = @_;
+
+  _check_attachments(@_) unless exists $pms->{olemacro_zip_password};
+
+  return $pms->{olemacro_zip_password};
+}
+
 sub _check_attachments {
 
   my ($self,$pms,$body,$name) = @_;
 
   my $mimec = 0;
+  my $chunk_size = $pms->{conf}->{olemacro_max_file};
 
   $pms->{olemacro_exists} = 0;
   $pms->{olemacro_malice} = 0;
+  $pms->{olemacro_renamed} = 0;
+  $pms->{olemacro_encrypted} = 0;
+  $pms->{olemacro_zip_password} = 0;
+  $pms->{olemacro_office_xml} = 0;
 
   foreach my $part ($pms->{msg}->find_parts(qr/./, 1)) {
 
-    # complex setup...
-
+    # setup vars and next if anything looks off
     my $ctt = $part->get_header('content-type');
     next unless defined $ctt;
-    my ($ctype) =
-      Mail::SpamAssassin::Util::parse_content_type($ctt);
+#    my ($ctype) =
+#      Mail::SpamAssassin::Util::parse_content_type($ctt);
     $ctt = _decode_part_header($part, lc($ctt || ''));
 
     my $name = '';
@@ -92,23 +210,22 @@ sub _check_attachments {
     }
 
     next if $name eq '';
-    next if ($name =~ $skip_exts);
+    next if ($name =~ qr/$pms->{conf}->{olemacro_skip_exts}/);
 
     my $cte = lc($part->get_header('content-transfer-encoding') || '');
     next unless ($cte =~ /^(?:base64)$/);
 
-    $ctype = lc $ctype;
-
+    #$ctype = lc $ctype;
     # we skipped what we need/want to
 
     my $data = undef;
 
     # if name is macrotype - return true
-    if ($name =~ $macrotypes) {
-      dbg("Found attachment with name $name of type $ctype ");
+    if ($name =~ qr/$pms->{conf}->{olemacro_macro_exts}/) {
+      dbg("Found macrotype attachment with name $name");# of type $ctype ");
       $pms->{olemacro_exists} = 1;
 
-      $data = $part->decode() unless defined $data;
+      $data = $part->decode($chunk_size) unless defined $data;
       if (_is_zip_file($name, $data)) {
         _check_zip($pms, $data);
       }
@@ -117,9 +234,38 @@ sub _check_attachments {
     }
 
     # if name is ext type - check and return true if needed
-    if ($name =~ $exts) {
-      dbg("Found attachment with name $name of type $ctype ");
-      $data = $part->decode() unless defined $data;
+    if ($name =~ qr/$pms->{conf}->{olemacro_exts}/) {
+      dbg("Found attachment with name $name");# of type $ctype ");
+      $data = $part->decode($chunk_size) unless defined $data;
+
+      # zipped doc that matches olemacro_exts - strange
+      if (_is_zip_file($name, $data)) {
+        dbg("$name is a zipped file");
+        if (_check_zip($pms, $data)) {
+          $pms->{olemacro_exists} = 1;
+          $pms->{olemacro_renamed} = $pms->{olemacro_office_xml};
+          return 1;
+        }
+      }
+
+      # if we find markers then return true
+      if (_check_markers($data)) {
+        $pms->{olemacro_exists} = 1;
+        if (_check_malice($data)) {
+          $pms->{olemacro_malice} = 1;
+        }
+        return 1;
+      }
+
+      if (_is_encrypted_doc($data)) {
+        dbg("Attachment $name is encrypted");
+        $pms->{olemacro_encrypted} = 1;
+      }
+    }
+
+    if ($name =~ qr/$pms->{conf}->{olemacro_zips}/) {
+      dbg("Found attachment with name $name");# of type $ctype ");
+      $data = $part->decode($chunk_size) unless defined $data;
 
       if (_is_zip_file($name, $data)) {
         dbg("$name is a zip file");
@@ -129,17 +275,40 @@ sub _check_attachments {
         }
       }
 
-      if (_check_markers($data)) {
-        $pms->{olemacro_exists} = 1;
-        if (_check_malice($data)) {
-          $pms->{olemacro_malice} = 1;
-        }
-        return 1;
-      }
     }
 
+    if ($pms->{conf}->{olemacro_extended_scan} == 1) {
+      dbg("Extended scan attachment with name $name");# of type $ctype ");
+      $data = $part->decode($chunk_size) unless defined $data;
+
+      if (_is_office_doc($data)) {
+        dbg("Found $name to be an Office Doc!");
+        $pms->{olemacro_renamed} = 1;
+        if (_check_markers($data)) {
+          $pms->{olemacro_exists} = 1;
+          if (_check_malice($data)) {
+            $pms->{olemacro_malice} = 1;
+          }
+          return 1;
+        }
+      }
+
+      if (_is_zip_file($name, $data)) {
+        dbg("$name is a zip file");
+        if (_check_zip($pms, $data)) {
+          $pms->{olemacro_exists} = 1;
+          return 1;
+        }
+      }
+
+    }
+
+    # if we get to here with data a part has been scanned nudge as reqd
     $mimec += 1 if defined $data;
-    last if $mimec > $max_mime;
+    if ($mimec > $pms->{conf}->{olemacro_num_mime}) {
+      dbg('MIME limit reached');
+      last;
+    }
 
   }
   return 0;
@@ -168,6 +337,7 @@ sub _check_zip {
 
   if (my $ctypesxml = $zip->memberNamed('[Content_Types].xml')) {
     dbg('Found [Content_Types].xml file');
+    $pms->{olemacro_office_xml} = 1;
     my ( $data, $status ) = $ctypesxml->contents();
     return 0 unless $status == AZ_OK;
 
@@ -189,22 +359,66 @@ sub _check_zip {
   # - check if a zip
   foreach my $member (@members){
     my $mname = lc $member->fileName();
-    next if $mname =~ $skip_exts;
+    next if ($mname =~ qr/$pms->{conf}->{olemacro_skip_exts}/);
 
     my $data = undef;
     my $status = undef;
 
     # if name is macrotype - return true
-    if ($mname =~ $macrotypes) {
+    if ($mname =~ qr/$pms->{conf}->{olemacro_macro_exts}/) {
+      dbg("Found macrotype zip member $mname");
+
+      if ($member->isEncrypted()) {
+        dbg("Zip member $mname is encrypted");
+        $pms->{olemacro_zip_password} = 1;
+        next;
+      }
+
       ( $data, $status ) = $member->contents() unless defined $data;
-      next unless $status == AZ_OK;
+      return 1 unless $status == AZ_OK;
+
       if (_is_zip_file($mname, $data)) {
         _check_zip($pms, $data);
       }
       return 1;
     }
 
-    if ($mname =~ $exts) {
+    if ($mname =~ qr/$pms->{conf}->{olemacro_exts}/) {
+      dbg("Found zip member $mname");
+
+      if ($member->isEncrypted()) {
+        dbg("Zip member $mname is encrypted");
+        $pms->{olemacro_zip_password} = 1;
+        next;
+      }
+
+      ( $data, $status ) = $member->contents() unless defined $data;
+      next unless $status == AZ_OK;
+
+      # zipped doc that matches olemacro_exts - strange
+      if (_is_zip_file($mname, $data)) {
+        dbg("$mname is a zipped file");
+        if (_check_zip($pms, $data)) {
+          $pms->{olemacro_renamed} = $pms->{olemacro_office_xml};
+          return 1;
+        }
+      }
+
+      if (_check_markers($data)) {
+        if (_check_malice($data)) {
+          $pms->{olemacro_malice} = 1;
+        }
+        return 1;
+      }
+
+      if (_is_encrypted_doc($data)) {
+        dbg("Attachment $mname is encrypted");
+        $pms->{olemacro_encrypted} = 1;
+      }
+
+    }
+
+    if ($mname =~ qr/$pms->{conf}->{olemacro_zips}/) {
       ( $data, $status ) = $member->contents() unless defined $data;
       next unless $status == AZ_OK;
 
@@ -214,17 +428,55 @@ sub _check_zip {
           return 1;
         }
       }
-
-      if (_check_markers($data)) {
-        return 1;
-        if (_check_malice($data)) {
-          $pms->{olemacro_malice} = 1;
-        }
-      }
     }
 
+    if ($pms->{conf}->{olemacro_extended_scan} == 1) {
+      ( $data, $status ) = $member->contents() unless defined $data;
+      next unless $status == AZ_OK;
+
+      dbg("Extended scan attachment with member name $mname");
+
+      if (_is_office_doc($data)) {
+        dbg("Found $mname to be an Office Doc!");
+        $pms->{olemacro_renamed} = 1;
+        if (_check_markers($data)) {
+          if (_check_malice($data)) {
+            $pms->{olemacro_malice} = 1;
+          }
+          return 1;
+        }
+      }
+
+      if (_is_zip_file($mname, $data)) {
+        dbg("$mname is a zip file");
+        if (_check_zip($pms, $data)) {
+          return 1;
+        }
+      }
+
+    }
+
+    # if we get to here with data a member has been scanned nudge as reqd
     $filec+=1 if defined $data;
-    last if $filec > $max_zip;
+    if ($filec > $pms->{conf}->{olemacro_num_zip}) {
+      dbg('Zip limit reached');
+      last;
+    }
+  }
+}
+
+sub _is_encrypted_doc {
+  my ($data) = @_;
+
+  if ((_is_office_doc($data)) && ($data =~ /(?:<encryption xmlns)/i)) {
+    return 1;
+  }
+}
+
+sub _is_office_doc {
+  my ($data) = @_;
+  if (index($data, $marker1) == 0) {
+    return 1;
   }
 }
 
@@ -233,7 +485,7 @@ sub _is_zip_file {
   if (index($data, 'PK') == 0) {
     return 1;
   } else {
-    return($name =~ /(?:zip)$/);    
+    return($name =~ /(?:zip)$/);
   }
 }
 
@@ -246,6 +498,7 @@ sub _check_markers {
   }
 
   if (index($data, 'w:macrosPresent="yes"') > -1) {
+    dbg('XML macros marker found');
     return 1;
   }
 }
@@ -256,12 +509,12 @@ sub _find_malice_bins {
   my @binfiles = $zip->membersMatching( '.*\.bin' );
 
   foreach my $member (@binfiles){
-      my ( $data, $status ) = $member->contents();
-      next unless $status == AZ_OK;
-      if (_check_malice($data)) {
-        return 1;
-      }
-  }  
+    my ( $data, $status ) = $member->contents();
+    next unless $status == AZ_OK;
+    if (_check_malice($data)) {
+      return 1;
+    }
+  }
 }
 
 sub _check_malice {
--- README.md
+++ README.md
@@ -12,34 +12,126 @@ The following Perl modules should be available
  - IO::String (libio-string-perl)
 
 ####Steps
+
  - Download zip / Clone repo
  - Move to contents to spamassassin location
 	 - Usually /etc/mail/spamassassin or /etc/spamassassin
- - Update OLEMacro.pre with correct path to the concepts directory
  - Restart Spamassassin
 
 ##Usage
-Two new body eval functions are added when this plug in is installed
+###Eval Rules
+New body eval functions are added when this plug in is installed
 
  - check_olemacro
  - check_olemacro_malice
+ - check_olemacro_renamed
+ - check_olemacro_encrypted
+ - check_olemacro_zip_password
 
-###check_olemacro
-Check for the existence of an attachment with an embedded OLE Macro
+####check_olemacro
+Check for the existence of an attachment with an embedded OLE Macro within files matching either *olemacro_exts* or *olemacro_macro_exts*
 
     body     OLEMACRO eval:check_olemacro()
-    describe OLEMACRO Attachment has a Office Macro
+    describe OLEMACRO Attachment has an Office Macro
     score    OLEMACRO 0.1
 
 
-###check_olemacro_malice
+####check_olemacro_malice
 Check for the existence of an attachment with a potential malicious embedded OLE Macro
 
     body     OLEMACRO_MALICE eval:check_olemacro_malice()
     describe OLEMACRO_MALICE Potentially malicious Office Macro
     score    OLEMACRO_MALICE 0.1
 
+####check_olemacro_renamed
+Check for the existence of an attachment that looks as though it is a *olemacro_macro_exts* file renamed to a *olemacro_exts* file
+
+    body     OLEMACRO_RENAME eval:check_olemacro_renamed()
+    describe OLEMACRO_RENAME Has an Office doc that has been renamed
+    score    OLEMACRO_RENAME 0.1
+
+
+####check_olemacro_encrypted
+Check if found Office document is encrypted
+
+    body     OLEMACRO_ENCRYPTED eval:check_olemacro_encrypted()
+    describe OLEMACRO_ENCRYPTED Has an Office doc that is encrypted
+    score    OLEMACRO_ENCRYPTED 0.1
+
+####check_olemacro_zip_password
+Check for the existence of an encrypted zip member that matches either *olemacro_exts* or *olemacro_macro_exts*
+
+    body     OLEMACRO_ZIP_PW eval:check_olemacro_zip_password()
+    describe OLEMACRO_ZIP_PW Has an Office doc that is password protected in a zip
+    score    OLEMACRO_ZIP_PW 0.1
+
+###Config Options
+All configuration options should be fine at default. Tweak at your own risk.
+
+ - olemacro_max_file
+ - olemacro_num_mime
+ - olemacro_num_zip
+ - olemacro_extended_scan
+ - olemacro_exts
+ - olemacro_macro_exts
+ - olemacro_zips
+ - olemacro_skip_exts
+
+####olemacro_max_file - [int] (bytes)
+Configure the largest file that the plugin will decode from the MIME objects
+#####Default
+
+    olemacro_max_file 512000
+
+####olemacro_num_mime - [int]
+Configure the maximum number of matching (see below) MIME parts the plugin will scan
+#####Default
+
+    olemacro_num_mime 5
+
+####olemacro_num_zip - [int]
+Configure the maximum number of matching (see below) zip members the plugin will scan
+#####Default
+
+    olemacro_num_zip 5
+
+####olemacro_extended_scan - [bool]
+Scan more files for potential macros, *olemacro_skip_exts* still honored
+**Note** this is off by default and shouldn't be needed. If this is turned on consider adjusting values for *olemacro_num_mime* and *olemacro_num_zip* and prepare for more CPU overhead
+#####Default
+
+    olemacro_extended_scan 0
+
+####olemacro_exts - [regex]
+Configure the extensions the plugin targets for macro scanning
+#####Default
+
+    olemacro_exts (?:doc|dot|pot|ppa|pps|ppt|xla|xls|xlt)$
+
+####olemacro_macro_exts - [regex]
+Configure the extensions the plugin treats as containing a macro
+#####Default
+
+    olemacro_macro_exts (?:docm|dotm|ppam|potm|ppst|ppsm|pptm|sldm|xlam|xlsb|xlsm|xltm)$
+
+####olemacro_zips
+Configure extensions for the plugin to target as zip files, files listed in configs above are also tested for zip
+#####Default
+
+    olemacro_zips (?:zip)$
+
+####olemacro_skip_exts - [regex]
+Configure extensions for the plugin to skip entirely, these should only be guaranteed macro free files
+#####Default
+
+     olemacro_skip_exts (?:docx|dotx|potx|pptx|xlsx)$
+
 ##Change Log
+###Version 0.4
+ - Added **check_olemacro_renamed**, **check_olemacro_zip_password** and **check_olemacro_encrypted**
+ - Added configuration options
+ - Adjusted zip support
+ - More dbg'ing
 ###Version 0.321
  - First public release
- - Thanks to Alex for his feedback
+ - Thanks to Alex for examples and testing
